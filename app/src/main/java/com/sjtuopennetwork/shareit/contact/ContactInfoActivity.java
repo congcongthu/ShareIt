b71c74c516e1ba1cb13dde7261f99211beeb41e8
impleman <49553740+impleman@users.noreply.github.com>
2019-10-20 19:06:27 +0800

    增加了联系人搜索和详情Activity

---
+++ app/src/main/java/com/sjtuopennetwork/shareit/contact/ContactInfoActivity.java
@@ -0,0 +1,15 @@
+package com.sjtuopennetwork.shareit.contact;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.sjtuopennetwork.shareit.R;
+
+public class ContactInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contact_info);
+    }
+}
