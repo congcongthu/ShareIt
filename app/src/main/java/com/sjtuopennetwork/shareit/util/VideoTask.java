abafe47d813635714af12b52f5d0845d5986963d
Riften <943275366@qq.com>
2019-11-11 23:05:57 +0800

    Do video segment and upload in a async way

--- app/src/main/java/com/sjtuopennetwork/shareit/util/VideoTask.java
+++ app/src/main/java/com/sjtuopennetwork/shareit/util/VideoTask.java
@@ -1,12 +1,103 @@
 package com.sjtuopennetwork.shareit.util;
 
+import android.media.MediaMetadataRetriever;
+import android.util.Log;
+
+import java.io.FileNotFoundException;
+
+import sjtu.opennet.hon.Handlers;
+import sjtu.opennet.textilepb.Model;
+
 /**
  * Video upload task.
  *
  */
 
 public class VideoTask {
+    //Variables get from constructor
     private String videoId;
     private String tsPath;
+    private boolean endTag;
+    private final String TAG = "VideoTask";
 
+    //Variables assigned during running.
+    private Model.VideoChunk videoChunk;
+
+    /**
+     * This handler is called by ipfsAddData
+     * It does the following things:
+     *  - create VideoChunk proto object
+     *  - add it to thread
+     *  - upload it to cafe peer
+     *
+     * @TODO:
+     * Use two blocking queue to do segment and upload.
+     */
+    private Handlers.IpfsAddDataHandler tsHandler = new Handlers.IpfsAddDataHandler() {
+        @Override
+        public void onComplete(String path) {
+            Log.d(TAG, String.format("ts chunk ipfs path: %s", path));
+            //Textile.instance().videos.addVideoChunk();
+        }
+
+        @Override
+        public void onError(Exception e) {
+            Log.e(TAG, String.format("ts upload failed"));
+            e.printStackTrace();
+        }
+    };
+
+    public static VideoTask endTask(){
+        return new VideoTask("","",true);
+    }
+
+    public VideoTask(String videoId, String tsPath, boolean endTag){
+        this.videoId = videoId;
+        this.tsPath = tsPath;
+        this.endTag = endTag;
+    }
+
+    /**
+     * The upload did following things:
+     *  - Read duration info [done]
+     *  - Upload to IPFS
+     *  - Create Video Chunk object
+     *  - Upload to Thread
+     *  - Publish to Cafe.
+     * @param currentDuration The duration now. Used as startTime;
+     * @return endTime. Used to update duration in video Uploader.
+     */
+    public int upload(int currentDuration){
+        if(endTag){
+            Log.d(TAG, "End task received. Return -1 to end the task thread.");
+            return -1;
+        }
+        int duration_int = 0;
+        MediaMetadataRetriever mdataReceiver = null;
+        try {
+            mdataReceiver = new MediaMetadataRetriever();
+            mdataReceiver.setDataSource(tsPath);
+            String duration = mdataReceiver.extractMetadata(MediaMetadataRetriever.METADATA_KEY_DURATION);
+
+            if (duration == null) {
+                Log.w(TAG, "Can not extract dutation.");
+                duration_int = -1;
+                return -1;
+            } else {
+                duration_int = Integer.parseInt(duration);
+            }
+            Log.d(TAG, String.format("Video Upload Execute, chunck duration: %d", duration_int));
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }finally{
+            //Important: Always release the retriever
+            if(mdataReceiver != null){
+                mdataReceiver.release();
+            }
+        }
+        return currentDuration + duration_int;
+        //Model.VideoChunk.newBuilder().
+
+    }
 }
--- app/src/main/java/com/sjtuopennetwork/shareit/util/VideoUploader.java
+++ app/src/main/java/com/sjtuopennetwork/shareit/util/VideoUploader.java
@@ -6,12 +6,15 @@ import java.util.concurrent.BlockingQueue;
 
 /**
  * Upload video in a async way.
+ * Each video will have its own VideoUploader (In order to count duration)
+ * Uploader should end when all the chunks has been uploaded.
  */
 
 public class VideoUploader extends Thread{
     private BlockingQueue<VideoTask> videoQueue;
     private final String TAG = "VideoUploader";
-
+    private int currentDuration = 0;
+    private boolean complete = false;
     public VideoUploader(BlockingQueue<VideoTask> bQueue){
         videoQueue = bQueue;
     }
@@ -21,16 +24,35 @@ public class VideoUploader extends Thread{
         interrupt();
     }
 
+
+
     @Override
     public void run(){
+        Log.d(TAG, "Uploader start to run.");
         VideoTask vTask;
-        try{
-            vTask = videoQueue.take();
-        }catch(InterruptedException ie){
-            Log.e(TAG, "Unexpected Interrupt.");
-            ie.printStackTrace();
-            interrupt();
-            
+        int endDuration;
+        while(!complete) {
+            try {
+                vTask = videoQueue.take();
+                endDuration = vTask.upload(currentDuration);
+                currentDuration = endDuration;
+                if(endDuration < 0){
+                    Log.d(TAG, "End Task received. End the thread.");
+                    //interrupt() should only apply to a blocking thread. So we use a flag "complete" instead.
+                    //interrupt();
+                    complete = true;
+                }
+            } catch (InterruptedException ie) {
+                Log.e(TAG, "Unexpected Interrupt.");
+                ie.printStackTrace();
+                interrupt();
+            } catch(Exception e){
+                e.printStackTrace();
+            }
+            //videoQueue.notify();
         }
+        Log.d(TAG, "Uploader end safely.");
     }
+
+
 }
