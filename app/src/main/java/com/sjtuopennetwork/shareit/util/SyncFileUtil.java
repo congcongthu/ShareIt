407d97be6f810abaccfc4df059cacf9d35d3ef38
Riften <943275366@qq.com>
2019-11-23 17:12:16 +0800

    fix bug about duration typr

--- app/src/main/java/com/sjtuopennetwork/shareit/util/SyncFileUtil.java
+++ app/src/main/java/com/sjtuopennetwork/shareit/util/SyncFileUtil.java
@@ -1,5 +1,7 @@
 package com.sjtuopennetwork.shareit.util;
 
+import android.util.Log;
+
 import com.google.protobuf.Timestamp;
 //import com.google.protobuf.util;
 import com.google.protobuf.util.Timestamps;
@@ -7,6 +9,7 @@ import com.google.protobuf.util.Timestamps;
 
 import java.util.Date;
 
+import sjtu.opennet.hon.Handlers;
 import sjtu.opennet.textilepb.Model.SyncFile;
 
 public class SyncFileUtil {
@@ -14,6 +17,24 @@ public class SyncFileUtil {
     String fileHash;
     String peerAddress;
     SyncFile.Type sType;
+
+    private Handlers.IpfsAddDataHandler posterHandler = new Handlers.IpfsAddDataHandler() {
+        @Override
+        public void onComplete(String path) {
+            synchronized (POSTERLOCK) {
+                Log.d(TAG, String.format("Poster ipfs path: %s", path));
+                posterHash = path;
+                POSTERLOCK.notify();
+            }
+        }
+
+        @Override
+        public void onError(Exception e) {
+            e.printStackTrace();
+            POSTERLOCK.notify();
+        }
+    };
+
     public SyncFileUtil(String filePath, String peerAddress, SyncFile.Type sType){
         this.filePath = filePath;
         this.peerAddress = peerAddress;
